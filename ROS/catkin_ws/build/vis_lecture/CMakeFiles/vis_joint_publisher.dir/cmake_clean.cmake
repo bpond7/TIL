f88b4cc30977a40e061fe4707eb2bbba0ff29256
bpond7 <j_bluesky@hotmail.co.jp>
2019-12-25 07:46:59 +0000

    learn tf

---
+++ ROS/catkin_ws/build/vis_lecture/CMakeFiles/vis_joint_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vis_joint_publisher.dir/src/joint_publisher1.cpp.o"
+  "/home/ubuntu/catkin_ws/devel/lib/vis_lecture/vis_joint_publisher.pdb"
+  "/home/ubuntu/catkin_ws/devel/lib/vis_lecture/vis_joint_publisher"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vis_joint_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ROS/catkin_ws/build/vis_lecture/CMakeFiles/vis_tf_listen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vis_tf_listen.dir/src/vis_tf_listen.cpp.o"
+  "/home/ubuntu/catkin_ws/devel/lib/vis_lecture/vis_tf_listen.pdb"
+  "/home/ubuntu/catkin_ws/devel/lib/vis_lecture/vis_tf_listen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vis_tf_listen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
