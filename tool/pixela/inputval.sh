5803bda19885eb9ef2dc9a2ffd3f055cf2cf22d9
bpond7 <j_bluesky@hotmail.co.jp>
2020-01-31 07:54:13 +0000

    try pixela

---
+++ tool/pixela/inputval.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20200113","quantity":"5"}'
+curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20200105","quantity":"4"}'
+curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20191230","quantity":"6"}'
+curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20191228","quantity":"5"}'
+curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20191222","quantity":"2"}'
+curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20191217","quantity":"3"}'
+curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20191214","quantity":"1"}'
+curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20191212","quantity":"1"}'
+curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20191211","quantity":"2"}'
+curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20191208","quantity":"2"}'
+
---
+++ tool/pixela/inputval2.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20191204","quantity":"2"}'
+
