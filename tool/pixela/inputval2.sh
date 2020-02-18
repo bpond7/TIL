ddaa62fbf0498aaf4e3a7b57649c5962846d7112
bpond7 <j_bluesky@hotmail.co.jp>
2020-02-18 08:19:40 +0000

    Arduino ROS setup

--- tool/pixela/inputval2.sh
+++ tool/pixela/inputval2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20191204","quantity":"2"}'
+curl -X POST https://pixe.la/v1/users/bpond7/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20200202","quantity":"3"}'
 
